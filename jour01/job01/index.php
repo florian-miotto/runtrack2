7752a9f37eaa872a07ac311b87b0784242c0ecef
florian-miotto <fmiotto@gmail.com>
2023-01-02 11:04:38 +0100

    job done

---
+++ jour01/job01/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+  $str = "LaPlateforme";
+  echo $str;
+  $str2="Vive";
+  $str3="!";
+
+  echo $str2 . " " . $str . " " . $str3;  
+
+  $val = 6;
+  echo $val;  
+  $val = $val + 4;
+  echo $val; 
+
+ 
+  $myBool = true;
+  echo $myBool;  
+  $myBool = false;
+  echo $myBool;  
+  
+
+?>
+</body>
+</html>
