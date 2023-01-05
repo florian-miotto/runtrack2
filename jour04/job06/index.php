f1b4dc54993b383dfc46e333fc4a5821728c7a0b
florian-miotto <fmiotto@gmail.com>
2023-01-05 12:56:59 +0100

    fonctionne avec break

--- jour04/job06/index.php
+++ jour04/job06/index.php
@@ -6,6 +6,41 @@ Après validation du formulaire :
 ● si la valeur entrée est un nombre pair, afficher “Nombre pair”,
 ● si c’est un nombre impair, afficher “Nombre impair”. -->
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method ="GET" id = "form" name = "form">
+<input type =text   name = "nombre" id = "nombre">
+<input type =submit>
+</form>    
+<?php
 
+
+
+
+
+
+while(isset($_GET['nombre'])) {
+  if($_GET['nombre'] % 2 == 0) {
+    echo "Nombre pair";
+    break;
+  }
+  else {
+    echo "Nombre impair";
+    break;
+
+  } 
+}
+?>
+
+
+</body>
+</html>
 
 
